89ed33dd58b739abaf09e29bf0819e43a2352296
Vladimir Podolian <dreamcore2219@gmail.com>
2016-03-29 19:03:12 +0300

    Connection to mongodb on MongoLab

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 import express from "express";
 
+import {MongoClient} from "mongodb";
+
 let app = express();
 
 app.use(express.static("public"));
 
 app.listen(3000);
+
+
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+  if (err) throw err;
+
+  database.collection("links").find({}).toArray((err, links) => {
+    if (err) throw err;
+
+    console.log(links);
+  });
+});
