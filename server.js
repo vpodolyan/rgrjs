ed0cd47be06bb3293c0e204b83cf231af1a6f28e
Vladimir Podolian <dreamcore2219@gmail.com>
2016-03-31 17:22:40 +0300

    Avoid hardcoded mongodb url

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ let app = express();
 
 app.use(express.static("public"));
 
+// MongoDb url is expected as first cli argument, default url otherwise
+let mongoUrl = process.argv[2] ? process.argv[2] : "mongodb://rgrjs:123456@ds025469.mlab.com:25469/rgrjs";
 let db
-MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+MongoClient.connect(mongoUrl, (err, database) => {
   if (err) throw err;
 
   db = database;
+  console.log(process.env.MONGO_URL);
   app.listen(3000, () => console.log("Listening on port 3000"));
 });
 
